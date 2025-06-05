0d1fbae7da60cb05e7b70291f6f191fc1231ce87
NanoProgrammer <satiagogonzalezjaimes77@gmail.com>
2025-06-05 09:43:18 -0600

    security

--- back-end/index.js
+++ back-end/index.js
@@ -11,7 +11,16 @@ const app = express();
 const AcceptedOrigin = ['https://simuxel.vercel.app', 'http://localhost:3000'];
 const PORT = process.env.PORT || 3000;
 const UserFunction = new UserModel();
+const API_KEY = process.env.API_SECRET ;
 
+
+app.use((req, res, next) => {
+  const key = req.headers['x-api-key'];
+  if (key !== API_KEY) {
+    return res.status(403).json({ error: 'Access denied' });
+  }
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use(MiddleCors({AcceptedOrigin}));
