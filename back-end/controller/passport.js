871d108d38e7b4873e9f9523c31d32acf11ae828
NanoProgrammer <satiagogonzalezjaimes77@gmail.com>
2025-06-07 11:10:34 -0600

    deploy of api

--- back-end/controller/passport.js
+++ back-end/controller/passport.js
@@ -7,7 +7,7 @@ const userModel = new UserModel();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-  callbackURL: '/auth/google/callback',
+  callbackURL: 'https://simuxel.onrender.com/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails[0].value;
