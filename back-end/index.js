01d8a4cf94dc140fd93f088e944f8fb9d168792c
NanoProgrammer <satiagogonzalezjaimes77@gmail.com>
2025-06-08 19:07:39 -0600

    backend error 4

--- back-end/index.js
+++ back-end/index.js
@@ -10,12 +10,12 @@ import AuthRouter from './routes/auth.js';
 
 const app = express();
 
-const AcceptedOrigin = ['https://simuxel.vercel.app/', 'http://localhost:3000'];
+const AcceptedOrigin = ['https://simuxel.vercel.app', 'http://localhost:3000'];
 const PORT = process.env.PORT || 3000;
 const UserFunction = new UserModel();
 const API_KEY = process.env.API_SECRET ;
 
-
+app.use(MiddleCors({AcceptedOrigin}));
 app.use((req, res, next) => {
   const key = req.headers['x-api-key'];
   const isValid =['/auth/google','/auth/google/callback'];
@@ -30,7 +30,6 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use(cookieParser());
-app.use(MiddleCors({AcceptedOrigin}));
 app.use('/users', UserRouter({ UserModel: UserFunction }));
 app.use('/auth', AuthRouter);
 
