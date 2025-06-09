c3bdd1f0caaebb3b261c585ec728a63e243999ac
NanoProgrammer <satiagogonzalezjaimes77@gmail.com>
2025-06-09 09:59:13 -0600

    /me

--- back-end/middleware/cors.js
+++ back-end/middleware/cors.js
@@ -1,17 +1,16 @@
 import cors from 'cors';
 
-export const MiddleCors =
-({AcceptedOrigin})=>{
-    return cors({
-  origin: (origin, callback) => {
-    if (!origin || AcceptedOrigin.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-   credentials: true,
-  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type','x-api-key']
-})
-}
+export const MiddleCors = ({ AcceptedOrigin }) => {
+  return cors({
+    origin: (origin, callback) => {
+      if (!origin || AcceptedOrigin.includes(origin)) {
+        callback(null, origin); // ✅ importante: no uses "true"
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'x-api-key'],
+  });
+};
