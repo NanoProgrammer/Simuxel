7f75edec19aaf29a1f2fa5be820cda35938378b3
NanoProgrammer <satiagogonzalezjaimes77@gmail.com>
2025-06-09 10:08:58 -0600

    /me

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -1,6 +1,6 @@
 // middleware/auth.js
 import jwt from 'jsonwebtoken';
-const SECRET_KEY = process.env.SECRECT_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
 
 export const requireAuth = (req, res, next) => {
   const token = req.cookies.access_token;
