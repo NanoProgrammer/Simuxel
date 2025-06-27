f5054b140c92e6f188e96d09934533d708d741aa
NanoProgrammer <satiagogonzalezjaimes77@gmail.com>
2025-06-27 08:26:31 -0600

    backend deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 4000);
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN || '*',
+  if(process.env.NODE_ENV === 'development') app.enableCors( {origin:"*", methods: "*", credentials: true } );
+  if(process.env.NODE_ENV === 'production') {
+    app.enableCors({
+    origin:  'https://simuxel.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  }
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
